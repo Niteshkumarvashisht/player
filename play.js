be4977a7d354b8b044adc5cb615c93a25262bd87
Nitesh Kumar Vashisht <nitesh@shooliniuniversity.com>
2025-04-26 16:35:12 +0530

    Update play.js

---
+++ play.js
@@ -0,0 +1,22 @@
+const songLinks = [
+  { 
+    url: "https://niteshkumarvashisht.github.io/player/BUNT., Mapei - TRIPPIN.mp3",
+    name: "BUNT., Mapei - TRIPPIN.mp3"
+  },
+  { 
+    url: "https://niteshkumarvashisht.github.io/player/Fred again.. & Baby Keem - leavemealone.mp3",
+    name: "Fred again.. & Baby Keem - leavemealone.mp3"
+  },
+  { 
+    url: "https://niteshkumarvashisht.github.io/player/ODESZA - A Moment Apart.mp3",
+    name: "ODESZA - A Moment Apart.mp3"
+  },
+  { 
+    url: "https://niteshkumarvashisht.github.io/player/ytmp3free.cc_baby-again-youtubemp3free.org.mp3",
+    name: "ytmp3free.cc_baby-again-youtubemp3free.org.mp3"
+  },
+  { 
+    url: "https://niteshkumarvashisht.github.io/player/ytmp3free.cc_tisto-1035-feat-tate-mcrae-official-music-video-youtubemp3free.org.mp3",
+    name: "ytmp3free.cc_tisto-1035-feat-tate-mcrae-official-music-video-youtubemp3free.org.mp3"
+  }
+];
